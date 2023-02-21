0979fb029bb870b438fc292d95987c9897888161
Anton Zuenko <anton@zuenko.net>
2023-02-21 17:54:39 +0100

    wip

--- nvim_fastnav.py
+++ nvim_fastnav.py
@@ -42,20 +42,24 @@ def get_winnr(socket_filename):
     sock.close()
     return winnr
 
-def encode_key_mapping(window, key_mapping):
-    mods, key = parse_shortcut(key_mapping)
-    event = KeyEvent(
-        mods=mods,
-        key=key,
-        shift=bool(mods & 1),
-        alt=bool(mods & 2),
-        ctrl=bool(mods & 4),
-        super=bool(mods & 8),
-        hyper=bool(mods & 16),
-        meta=bool(mods & 32),
-    ).as_window_system_event()
+def encode_key_mappings(window, key_mappings):
+    events = []
+    for key_mapping in key_mappings:
+        mods, key = parse_shortcut(key_mapping)
+        event = KeyEvent(
+            mods=mods,
+            key=key,
+            shift=bool(mods & 1),
+            alt=bool(mods & 2),
+            ctrl=bool(mods & 4),
+            super=bool(mods & 8),
+            hyper=bool(mods & 16),
+            meta=bool(mods & 32),
+        ).as_window_system_event()
 
-    return window.encoded_key(event)
+        events.append(window.encoded_key(event))
+
+    return events
 
 
 def main():
@@ -65,20 +69,31 @@ def main():
 @result_handler(no_ui=True)
 def handle_result(args, result, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
-    direction = args[1]
-    key_mapping = args[2]
+    command = args[1]
+    key_mappings = args[2].split(',')
     vim_id = args[3] if len(args) > 4 else "n?vim"
 
     if window is None:
         return
-    if socket_filename := get_nvim_socket(window, vim_id):
-        encoded = encode_key_mapping(window, key_mapping)
-        old_winnr = get_winnr(socket_filename)
-        window.write_to_child(encoded)
-        time.sleep(.030)
-        new_winnr = get_winnr(socket_filename)
-        if old_winnr == new_winnr:
-            boss.active_tab.neighboring_window(direction)
-    else:
-        boss.active_tab.neighboring_window(direction)
 
+    if command.startswith('neighboring_window_'):
+        direction = command[len('neighboring_window_'):]
+
+        if socket_filename := get_nvim_socket(window, vim_id):
+            encoded_key_events = encode_key_mappings(window, key_mappings)
+            old_winnr = get_winnr(socket_filename)
+            for eke in encoded_key_events:
+                window.write_to_child(eke)
+            time.sleep(.030)
+            new_winnr = get_winnr(socket_filename)
+            if old_winnr == new_winnr:
+                boss.active_tab.neighboring_window(direction)
+        else:
+            boss.active_tab.neighboring_window(direction)
+    elif command == 'close_window_with_confirmation':
+        if socket_filename := get_nvim_socket(window, vim_id):
+            encoded_key_events = encode_key_mappings(window, key_mappings)
+            for eke in encoded_key_events:
+                window.write_to_child(eke)
+        else:
+            boss.close_window_with_confirmation(ignore_shell = True)
